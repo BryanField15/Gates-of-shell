29d1de3e0c3984eb04f887f22d23d61aa1dd4600
BryanField15 <6367@holbertonstudents.com>
2023-04-21 04:52:14 -0700

    task 9 solution

---
+++ 9-fork_wait_execve.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - creates 5 child processes
+ *
+ *
+ *Return: 0
+ */
+
+int main()
+{
+	pid_t child_pid;
+	int i;
+	char *cmd[] = {"ls", "-l", "/tmp", NULL};
+	char *envp[] = {NULL};
+
+	i = 1;
+
+	while (i < 5)
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error: fork failed");
+			exit(1);
+		}
+		else if (child_pid == 0)
+		{
+			printf("Child %d is executing\n", i);
+			execve("/bin/ls", cmd, envp);
+			exit(0);
+		}
+		else
+		{
+			wait(NULL);
+			printf("Child %d exited\n\n", i);
+		}
+		i = i + 1;
+	}
+	return (0);
+}
